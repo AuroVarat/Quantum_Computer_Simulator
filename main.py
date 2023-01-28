95402c6425defb0abe7b4ddedaea70b9c7181b54
AuroVarat <aurovaratpatnaik@gmail.com>
2023-01-28 02:12:08 +0000

    ndimensional basis

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from CircuitClass import QbitRegister
-from CircuitClass import Gate as g
+from ComponentClass import QbitRegister
 
 import numpy as np
 
-input_file = np.genfromtxt('input.dat', delimiter=',', dtype=int)
+# input_file = np.genfromtxt('input.dat', delimiter=',', dtype=int)
 
-# Pure State Network check the image in the folder
-register = QbitRegister(input_file, name = "4PureNetwork") #initialise qbit register
-h = g.hadamard(register,(1,)) # apply hadamard gate to qbit 1
-p = g.phase_shift(h,(1,),2*np.pi) #apply phase shift to qbit 1
-p = lambda phi: g.phase_shift(p,(1,),phi+np.pi/2) #apply phase shift to qbit 1
-
+# # Pure State Network check the image in the folder
+register = QbitRegister(2, name = "4PureNetwork") #initialise qbit register
+register.hadamard(0) # apply hadamard gate to qbit 1
+register.phase_shift(0,2*np.pi) #apply phase shift to qbit 1
+lambda theta : register.phase_shift(0,theta) #apply phase shift to qbit 1
 
 #EPR Paradox/ uncomment below to run
-# register = QbitRegister([0,0], name ="4EPR") #initialise qbit register
-# h = g.hadamard(register,(1,)) # apply hadamard gate to qbit 1
-# cnot = g.cnot(h,[1,2]) #apply cnot gate with qbit 1 as target and 2 as control
+# register = QbitRegister(2, name ="4EPR") #initialise qbit register
+# register.hadamard(0) # apply hadamard gate to qbit 1
+# register.cnot(0) #apply cnot gate with qbit 1,2 as control and target respectively
+# print(register)
